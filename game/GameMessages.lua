678c9b18b81e54ab352e7433459033ccc75fe4ab
expecter <1005426244@qq.com>
2017-12-11 14:34:44 +0800

    1
    1

---
+++ game/GameMessages.lua
@@ -0,0 +1,10 @@
+--
+-- Author: Your Name
+-- Date: 2017-12-01 21:48:37
+--
+local M = {
+	--hero
+	hero = "hero"
+}
+
+return M
---
+++ test.lua
@@ -0,0 +1,7 @@
+--
+-- Author: Your Name
+-- Date: 2017-12-07 14:16:53
+--
+local a = "asdfa"
+local b = "a"
+print(string.find(a,b))
